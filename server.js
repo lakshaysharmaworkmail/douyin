b12160c0839eb5117c4ebe4f54e1d1eab1803331
lakshaysharmaworkmail <lakshaysharmaworkmail@gmail.com>
2026-06-26 10:37:00 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const axios = require("axios");
 const path = require("path");
 const fs = require("fs");  
+const app = express();  
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
